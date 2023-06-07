b3b994e1f472e5c02e931e653ff7a4d9f344ec76
Morten Victor Nordbye <morten@nordbye.it>
2023-06-07 20:11:29 +0200

    Update improved-users1.ps1

--- improved-users1.ps1
+++ improved-users1.ps1
@@ -1,26 +1,30 @@
-# Import Active Directory module for running AD cmdlets
+# Import the Active Directory module to provide the cmdlets we need
 Import-Module ActiveDirectory
 
-# Import user data from CSV
-$ADUsers = Import-Csv -Delimiter ";" C:\Users\Administrator\Documents\users.csv
+# Import user data from the CSV file located in the same directory as the script
+$ADUsers = Import-Csv -Delimiter ";" .\users.csv
 
-# Process each user in the CSV file
+# Process each user in the CSV file one by one
 foreach ($User in $ADUsers) {
+    # Output a message indicating which user is currently being processed
     Write-Host "Processing user: $($User.username)"
 
-    # Assign user properties from CSV
+    # Assign user properties from the current line of the CSV file
     $Username   = $User.username
     $Password   = $User.password
     $Firstname  = $User.firstname
     $Lastname   = $User.lastname
     $OU         = $User.ou
+    $Group      = $User.groupname
 
-    # Check if the user already exists in Active Directory
+    # Check if a user with the current username already exists in Active Directory
     if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+        # If user exists, write a warning message to the console
         Write-Warning "A user account with username $Username already exists in Active Directory."
     } else {
+        # If user does not exist, try to create a new user
         try {
-            # Create a new AD user with the provided details
+            # Create a new user in Active Directory with the provided details
             New-ADUser `
                 -SamAccountName $Username `
                 -Name "$Firstname $Lastname" `
@@ -32,11 +36,14 @@ foreach ($User in $ADUsers) {
                 -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
                 -ChangePasswordAtLogon $True
 
-            # Confirm user creation and continue
-            Read-Host "Successfully added account with username $Username. Press Enter to continue."
+            # Add the newly created user to the specified group
+            Add-ADGroupMember -Identity $Group -Members $Username
+
+            # Output a message confirming successful user creation and group addition
+            Read-Host "Successfully added account with username $Username to group $Group. Press Enter to continue."
         } catch {
-            # Handle any errors during user creation
-            Write-Error "An error occurred while creating user $Username: $_"
+            # If there's an error during the user creation process, write the error to the console
+            Write-Error "An error occurred while creating user ${Username}: $_"
         }
     }
 }
